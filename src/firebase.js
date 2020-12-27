bae13956554794000838d51e14472d70ea62bfdb
Denis <k1selevde@yandex.ru>
2020-12-27 18:56:13 +0500

    clean firebase

--- src/firebase.js
+++ src/firebase.js
@@ -15,12 +15,3 @@ firebase.initializeApp(firebaseConfig)
 
 
 export default firebase;
-
-// const firebaseConfig = {
-//     apiKey: process.env["API_KEY "]"AIzaSyBfpOjj-qiDj0bBchciobeVJ_YQrD7SvLM",
-//     authDomain: "react-example-10ad3.firebaseapp.com",
-//     projectId: "react-example-10ad3",
-//     storageBucket: "react-example-10ad3.appspot.com",
-//     messagingSenderId: "609191692591",
-//     appId: "1:609191692591:web:ba1c17b3c94c44593079da"
-// };
